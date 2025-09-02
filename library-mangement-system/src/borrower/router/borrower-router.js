afd79ddb6d57685c3e4d0a3777bde940e2ca2681
mahmoud adel <mahmoud.adel@avidbeam.com>
2025-09-02 15:51:16 +0300

    feat: finish the borrowers apis

--- library-mangement-system/src/borrower/router/borrower-router.js
+++ library-mangement-system/src/borrower/router/borrower-router.js
@@ -1,13 +1,49 @@
 import { Router } from "express";
 import BorrowerController from "../controller/borrower-controller.js";
-import { findAllBorrowersSchema } from "../validation/borrower-validation.js";
+import {
+  findAllBorrowersSchema,
+  deleteBorrowerSchema,
+  signUpBorrowerSchema,
+  loginBorrowerSchema,
+  updateBorrowerParamsSchema,
+  updateBorrowerBodySchema,
+} from "../validation/borrower-validation.js";
 import { validateRequest } from "../../shared/middleware/request-validator.js";
+import { authenticateToken } from "../../shared/middleware/authenticator.js";
 
 const BorrowerRouter = Router();
+
 BorrowerRouter.get(
   "/",
   validateRequest(findAllBorrowersSchema, "query"),
   BorrowerController.findAll
+);
+
+BorrowerRouter.patch(
+  "/:id",
+  authenticateToken,
+  validateRequest(updateBorrowerParamsSchema, "params"),
+  validateRequest(updateBorrowerBodySchema),
+  BorrowerController.update
+);
+
+BorrowerRouter.delete(
+  "/:id",
+  authenticateToken,
+  validateRequest(deleteBorrowerSchema, "params"),
+  BorrowerController.delete
+);
+
+BorrowerRouter.post(
+  "/sign-up",
+  validateRequest(signUpBorrowerSchema),
+  BorrowerController.signUp
+);
+
+BorrowerRouter.post(
+  "/login",
+  validateRequest(loginBorrowerSchema),
+  BorrowerController.login
 );
 
 export default BorrowerRouter;
